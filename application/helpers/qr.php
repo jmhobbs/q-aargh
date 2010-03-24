f6c74f58774ab36fa935929900bda63c12b64361
John Hobbs <john@velvetcache.org>
2010-03-24 11:05:38 -0500

    Updated QR helper.

--- application/helpers/qr.php
+++ application/helpers/qr.php
@@ -17,18 +17,26 @@
 			//! \todo UTF-8 conversion or url?
 			$url = "http://chart.apis.google.com/chart?chs={$width}x{$height}&cht=qr&chl=" .urlencode( Kohana::config( 'core.site_domain' ) . $island ). "&choe=UTF-8%chld=L|4";
 
+			$island = sha1( $island );
 			$cache_dir = Kohana::config( 'qaargh.qr_directory' );
 			$subdir = substr( $island, 0, 1);
 			$cache_file = "{$cache_dir}/{$subdir}/{$island}_{$width}x{$height}.png";
 			
 			if( ! file_exists( $cache_file ) ) {
-				$curl = Curl::factory( $url );
-				$result = $curl->execute();
+				
+				$ch = curl_init();
+				curl_setopt( $ch, CURLOPT_URL, $url );
+				curl_setopt( $ch, CURLOPT_HEADER, 0 );
+				curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
+				$result = curl_exec( $ch );
+				curl_close( $ch );
+				
 				if( false === $result )
 					return false;
-				else
-					if( false === file_put_contents( $cache_file, $result, FILE_BINARY ) )
-						return false;
+				
+				if( false === file_put_contents( $cache_file, $result ) )
+					return false;
+
 			}
 			
 			return $cache_file;
