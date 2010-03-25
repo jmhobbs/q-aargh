ad55c2bf5596f844c91d65f4b0b57633c5d94e78
John Hobbs <john@velvetcache.org>
2010-03-25 12:38:43 -0500

    Config file template changes.

--- application/config/qaargh.php
+++ application/config/qaargh.php
@@ -1,5 +1,5 @@
 <?php
 	$config['qr_directory'] = DOCROOT . 'qr/';
-	$config['confirm_salt'] = 'iuahsiyarv';
+	$config['confirm_salt'] = 'CHANGE_ME';
 	$config['mailer'] = 'no-reply@localhost';
 	$config['homepage'] = 'home';
