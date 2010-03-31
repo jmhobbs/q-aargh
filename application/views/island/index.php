cdd8f4f4fa6839c6dbc7a2e46d972159f84cde38
John Hobbs <john@velvetcache.org>
2010-03-30 22:34:32 -0500

    Improved island display.

--- application/views/island/index.php
+++ application/views/island/index.php
@@ -1,6 +1,14 @@
+<fieldset class="island-stats">
+	<legend>Island Stats</legend>
+	<label>Owner:</label> <?= html::anchor( '/user/view/' . html::specialchars( $island->user->username ), html::specialchars( $island->user->username ) ); ?><br/>
+	<label>Visits:</label> <?= $island->visits() ?><br/>
+	<label>Views:</label> <?= $island->views ?><br/>
+</fieldset>
+
+<h2>Message From The Founder</h2>
 <?= text::auto_p( html::specialchars( $island->introduction ) ); ?>
 
-<h2>Posts</h2>
+<h2>Subsequent Posts</h2>
 <?php
 	foreach( $island->posts() as $post ) {
 		$view = new View( 'post/' . $post->type );
